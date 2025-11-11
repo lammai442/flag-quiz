adba08a99b99ee12a3f3ffdc0400c9579cb7df45
lammai442 <lam.mai442@gmail.com>
2025-11-11 20:03:31 +0100

    Removed comments

--- dist/data/index.js
+++ dist/data/index.js
@@ -25,18 +25,6 @@ export const highScoreMockup = [
         totalHelp: 8,
     },
 ];
-// export const updateLocalStorage = (
-// 	newGamePlayer: NewGamePlayer
-// ): NewGamePlayer[] => {
-// 	const highScoreData: NewGamePlayer[] = JSON.parse(
-// 		localStorage.getItem('highScore') || JSON.stringify(highScoreMockup)
-// 	);
-// 	highScoreData.push(newGamePlayer);
-// 	sortingHighScore(highScoreData);
-// 	const topFive = highScoreData.slice(0, 5);
-// 	localStorage.setItem('highScore', JSON.stringify(topFive));
-// 	return topFive;
-// };
 export const oGameData = {
     nmbrOfCountries: 10,
     questionNmbr: 1,
--- src/data/index.ts
+++ src/data/index.ts
@@ -28,24 +28,6 @@ export const highScoreMockup: NewGamePlayer[] = [
 	},
 ];
 
-// export const updateLocalStorage = (
-// 	newGamePlayer: NewGamePlayer
-// ): NewGamePlayer[] => {
-// 	const highScoreData: NewGamePlayer[] = JSON.parse(
-// 		localStorage.getItem('highScore') || JSON.stringify(highScoreMockup)
-// 	);
-
-// 	highScoreData.push(newGamePlayer);
-
-// 	sortingHighScore(highScoreData);
-
-// 	const topFive = highScoreData.slice(0, 5);
-
-// 	localStorage.setItem('highScore', JSON.stringify(topFive));
-
-// 	return topFive;
-// };
-
 export const oGameData: GameData = {
 	nmbrOfCountries: 10,
 	questionNmbr: 1,
